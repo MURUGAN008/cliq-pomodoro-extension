2a362065f24f1f2e2b8d99ac0c2cdd79abef2881
S. MURUGAN <114326266+MURUGAN008@users.noreply.github.com>
2025-11-28 20:59:09 +0530

    Delete functions directory

--- functions/practice_zh_function/routes/startRoute.js
+++
@@ -1,7 +0,0 @@
-const path=require("path");
-const startController=require(path.join(__dirname,"..","controllers","startController.js"));
-const express=require("express");
-const route=express.Router();
-route.post("/",startController);
-
-module.exports=route;
